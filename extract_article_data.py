20573bb5e6c9e50ef2a3011c640b3117bb553fd7
Qixiang Fang <fqixiang@gmail.com>
2024-07-05 00:25:36 +0200

    Update extract_article_data.py

--- extract_article_data.py
+++ extract_article_data.py
@@ -2,6 +2,8 @@ import zipfile
 import polars as pl
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
+import os
+
 
 # Path to delpher's zip folder
 file_path = 'C:/Users/fqixi/datasets/kranten_pd_1860-4.zip'
@@ -13,7 +15,7 @@ results = []
 with zipfile.ZipFile(file_path, 'r') as zip_ref:
     # Iterate through each file in the zip archive
     for file_info in tqdm(zip_ref.infolist()):
-        filename = file_info.filename
+        filename = os.path.basename(file_info.filename)
         publication_date = filename[:10]
         if filename.endswith('articletext.xml'):
             with zip_ref.open(file_info) as file:
@@ -25,21 +27,19 @@ with zipfile.ZipFile(file_path, 'r') as zip_ref:
                     p_contents = [p.text for p in root.findall('p')]
                     p_contents = ' '.join(p_contents)
                     results.append({
-                        'file_name': file_info.filename,
-                        'date': publication_date,
+                        'file_name': filename,
                         'title': title_content,
-                        'paragraph': p_contents,
+                        'text': p_contents,
                     })
                 except Exception as e:
                     results.append({
                         'file_name': file_info.filename,
-                        'date': publication_date,
                         'title': f"An error occurred: {str(e)}",
-                        'paragraph': f"An error occurred: {str(e)}",
+                        'text': f"An error occurred: {str(e)}",
                     })
 
 # Convert the results to a Polars DataFrame
 df = pl.DataFrame(results)
 
 # Save the DataFrame as a Parquet file
-df.write_parquet('delpher.parquet')
+df.write_parquet('article_data.parquet')
