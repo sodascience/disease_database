a2f37a964fd3a7f9ac59c146c19db2cfd27ffb13
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2024-10-16 09:37:18 +0200

    new combine scripts first version
    WIP

--- data_conversion/combine_and_chunk.py
+++ data_conversion/combine_and_chunk.py
@@ -4,46 +4,81 @@ folder for performing data analysis.
 """
 import polars as pl
 from tqdm import tqdm
-import argparse
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--start_year", type=int, default=1830)
-    parser.add_argument("--end_year", type=int, default=1879)
+out_folder = "processed_data/combined"
 
-    args = parser.parse_args()
-    start_year = args.start_year
-    end_year = args.end_year
-
-    out_folder = "processed_data/combined"
-
-    article_text_df = pl.scan_parquet(f"processed_data/texts/from_{start_year}_to_{end_year}/*.parquet")
-    article_meta_df = pl.scan_parquet(f"processed_data/metadata/articles/from_{start_year}_to_{end_year}/*.parquet")
-    newspaper_meta_df = pl.scan_parquet(f"processed_data/metadata/newspapers/from_{start_year}_to_{end_year}/*.parquet")
-
-    article_text_df.head().collect()
-    article_meta_df.head().collect()
-    newspaper_meta_df.head().collect()
-
-    # create master df with everything needed
-    final_df = article_meta_df.join(
-        article_text_df,
-        on="article_id",
-        how="left",
-    ).join(newspaper_meta_df, on="newspaper_id", how="left")
-
-    # write to chunked parquet files
-    year_chunksize = 1
-    for start_year in tqdm(range(start_year, end_year, year_chunksize)):
-        end_year = start_year + year_chunksize
-        final_df.filter(
-            pl.col("newspaper_date").dt.year() >= start_year, pl.col("newspaper_date").dt.year() < end_year
-        ).sink_parquet(f"{out_folder}/combined_{start_year}_{end_year}.parquet")
+article_text_df = pl.scan_parquet("processed_data/texts/*.parquet")
+article_meta_df = pl.scan_parquet("processed_data/metadata/articles/*.parquet")
+journal_meta_df = pl.scan_parquet("processed_data/metadata/journals/*.parquet")
 
 
-    df_final = pl.scan_parquet("processed_data/combined/*.parquet")
-    df_final.head().collect()
-    df_final.collect_schema()
+article_text_df.head().collect()
+article_meta_df.head().collect()
+journal_meta_df.head().collect()
 
-if __name__ == "__main__":
-    main()
+# compute journal_id and article_id
+# for article text
+article_text_df = (
+    article_text_df.with_columns(
+        pl.col("file_name")
+        .str.split_exact("_", 2)
+        .struct.rename_fields(["ddd", "journal_id", "article_num"])
+        .alias("fields")
+    )
+    .unnest("fields")
+    .with_columns(
+        (pl.col("journal_id") + "_" + pl.col("article_num")).alias("article_id")
+    )
+    .select("journal_id", "article_id", "title", "text")
+)
+
+# for article meta
+article_meta_df = (
+    article_meta_df.with_columns(
+        pl.col("item_filename")
+        .str.split_exact("_", 2)
+        .struct.rename_fields(["ddd", "journal_id", "article_num"])
+        .alias("fields")
+    )
+    .unnest("fields")
+    .with_columns(
+        (pl.col("journal_id") + "_" + pl.col("article_num")).alias("article_id")
+    )
+    .filter(pl.col("item_filename").is_not_null())
+    .select("journal_id", "article_id", "item_subject", "item_type")
+)
+
+# for journal meta
+journal_meta_df = (
+    journal_meta_df.with_columns(
+        pl.col("newspaper_id")
+        .str.split_exact(":", 1)
+        .struct.rename_fields(["ddd", "journal_id"])
+        .alias("fields")
+    )
+    .unnest("fields")
+    .with_columns(pl.col("newspaper_date").alias("date"))
+    .select(pl.exclude("newspaper_id"))
+    .select("journal_id", "date", pl.selectors.starts_with("newspaper"), "pdf_link")
+)
+
+
+# create master df with everything needed
+final_df = article_text_df.join(
+    article_meta_df.select(pl.exclude("journal_id")),
+    on="article_id",
+    how="left",
+).join(journal_meta_df, on="journal_id", how="left")
+
+# write to chunked parquet files
+year_chunksize = 1
+for startyr in tqdm(range(1830, 1880, year_chunksize)):
+    endyr = startyr + year_chunksize
+    final_df.filter(
+        pl.col("date").dt.year() >= startyr, pl.col("date").dt.year() < endyr
+    ).sink_parquet(f"{out_folder}/combined_{startyr}_{endyr}.parquet")
+
+
+df_final = pl.scan_parquet("processed_data/combined/*.parquet")
+df_final.head().collect()
+df_final.collect_schema()
---
+++ delpher_api/combine_and_chunk.py
@@ -0,0 +1,58 @@
+import polars as pl
+from pathlib import Path
+
+ARTICLE_ID_FOLDER = Path("processed_data", "metadata", "articles", "monthly_ids")
+ARTICLE_TEXT_FOLDER = Path("processed_data", "texts", "monthly_texts")
+NEWSPAPER_META_FOLDER = Path("processed_data", "metadata", "newspapers", "monthly")
+
+
+article_ids = pl.scan_parquet(ARTICLE_ID_FOLDER / "*.parquet")
+article_texts = pl.scan_parquet(ARTICLE_TEXT_FOLDER / "*.parquet")
+journal_meta = pl.scan_parquet(NEWSPAPER_META_FOLDER / "*.parquet")
+
+# transform article ids
+article_df = (
+    article_ids.with_columns(
+        pl.col("article_id")
+        .str.split_exact(":", 4)
+        .struct.rename_fields(
+            ["journal_archive", "journal_id", "filetype", "article_num"]
+        )
+        .alias("fields")
+    )
+    .unnest("fields")
+    .with_columns(
+        (pl.col("journal_id") + "_" + pl.col("article_num").str.extract("(\\d+)")).alias("article_id")
+    )
+    .select("journal_id", "article_id", "article_subject")
+)
+
+# transform ids in texts
+text_df = (
+    article_texts.with_columns(
+        pl.col("article_id")
+        .str.split_exact(":", 4)
+        .struct.rename_fields(
+            ["journal_archive", "journal_id", "filetype", "article_num"]
+        )
+        .alias("fields")
+    )
+    .unnest("fields")
+    .with_columns(
+        (pl.col("journal_id") + "_" + pl.col("article_num").str.extract("(\\d+)")).alias("article_id"),
+        pl.col("article_title").alias("title"),
+        pl.col("article_text").alias("text")
+    )
+    .select("article_id", "title", "text")
+)
+
+journal_df = (
+
+)
+
+final_df = article_ids.join(
+    article_texts, on="article_id", how="left", coalesce=True
+).join(journal_meta, on="newspaper_id", how="left", coalesce=True)
+
+pl.scan_parquet("processed_data/combined/*.parquet").head().collect()
+
