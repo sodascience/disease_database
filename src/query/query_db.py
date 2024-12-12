1e5a8b24a1430005c0d269ee326e7b73bb78eba9
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2024-12-12 14:38:37 +0100

    query for amsterdam, dordrecht, and groningen

--- src/query/query_db.py
+++ src/query/query_db.py
@@ -4,7 +4,7 @@ import plotnine as p9
 
 df = (
     pl.read_parquet("processed_data/database/**/*.parquet")
-    .filter(pl.col("location") == "Amsterdam")#, pl.col("disease").is_in(["cholera", "smallpox"]))
+    .filter(pl.col("location").is_in(["Amsterdam", "Groningen", "Dordrecht"]))#, pl.col("disease").is_in(["cholera", "smallpox"]))
     .with_columns(pl.date(pl.col("year"), pl.col("month"), 1).alias("date"))
 )
 
@@ -23,15 +23,15 @@ plt = (
     + p9.geom_ribbon(alpha=0.4, color="none")
     + p9.geom_line()
     + p9.scale_x_date(date_breaks="10 years", date_labels="%Y")
-    + p9.facet_wrap("disease", scales="free")
+    + p9.facet_grid(rows="disease", cols="location", scales="free")
     + p9.theme_linedraw()
     + p9.theme(axis_text_x=p9.element_text(rotation=90), legend_position="none")
     + p9.labs(
-        title="Disease mentions in Amsterdam, 1830 - 1940",
+        title="Disease mentions in Amsterdam, Dordrecht, Groningen, 1830 - 1940",
         subtitle="Data from Delpher newspaper archive, Royal Library",
         x="Year",
         y = "Normalized mentions"
     )
 )
 
-p9.ggsave(plt, "img/amsterdam_all.png", width=15, height=9, dpi=300)
+p9.ggsave(plt, "img/adg_all.png", width=10, height=15, dpi=300)
